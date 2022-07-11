1230115d30be378806ebae2a903a1752ad28c35b
Valeriy Manzhos <47538801+InvisibleRain@users.noreply.github.com>
2022-07-11 21:10:24 +0300

    Finally make start-snap.sh readable

--- start-snap.sh
+++ start-snap.sh
@@ -1,12 +1,13 @@
 #/bin/bash
 
-sudo /bin/bash -c "\
-echo 'Please wait...';\
-sleep 10;
-snap connect v2raya:firewall-control;\
-snap connect v2raya:network;\
-snap connect v2raya:network-bind;\
-snap connect v2raya:network-control;\
-snap connect v2raya:network-observe;\
-snap connect v2raya:v2raya-files;\
-snap start v2raya;"
+sudo /bin/bash <<EOF
+echo 'Please wait...'
+sleep 10
+snap connect v2raya:firewall-control
+snap connect v2raya:network
+snap connect v2raya:network-bind
+snap connect v2raya:network-control
+snap connect v2raya:network-observe
+snap connect v2raya:v2raya-files
+snap start v2raya
+EOF
