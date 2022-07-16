19db4f6465285509af8c56fddc34418bba0fe4ae
Valeriy Manzhos <47538801+InvisibleRain@users.noreply.github.com>
2022-07-16 11:46:47 +0300

    Little comment

--- start-snap.sh
+++ start-snap.sh
@@ -2,7 +2,7 @@
 
 sudo /bin/bash <<EOF
 echo 'Please wait...'
-sleep 10
+sleep 10 # Launching snap instantly after the installation sometimes breaks, so it's better to play safe
 snap connect v2raya:firewall-control
 snap connect v2raya:network
 snap connect v2raya:network-bind
