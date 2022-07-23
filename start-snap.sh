1c6b39726c3c872f7a545c7974010e34a12db847
Valeriy Manzhos <47538801+InvisibleRain@users.noreply.github.com>
2022-07-23 19:17:54 +0300

    Remove unnecessary connections from script

--- start-snap.sh
+++ start-snap.sh
@@ -4,9 +4,6 @@ sudo /bin/bash <<EOF
 echo 'Please wait...'
 sleep 10 # Launching snap instantly after the installation sometimes breaks, so it's better to play safe
 snap connect v2raya:firewall-control
-snap connect v2raya:network
-snap connect v2raya:network-bind
 snap connect v2raya:network-control
-snap connect v2raya:network-observe
 snap start v2raya
 EOF
