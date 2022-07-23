ae104cc8adcd692774b9dd1b1c13ee51e4a3ce07
Valeriy Manzhos <47538801+InvisibleRain@users.noreply.github.com>
2022-07-23 19:21:37 +0300

    Remove delay from script

--- start-snap.sh
+++ start-snap.sh
@@ -1,8 +1,6 @@
 #/bin/bash
 
 sudo /bin/bash <<EOF
-echo 'Please wait...'
-sleep 10 # Launching snap instantly after the installation sometimes breaks, so it's better to play safe
 snap connect v2raya:firewall-control
 snap connect v2raya:network-control
 snap start v2raya
