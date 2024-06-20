e18c9f007b6d63e00f31f0beb4f9b9387ea634b0
Valeriy Manzhos <47538801+InvisibleRain@users.noreply.github.com>
2024-06-20 14:05:43 +0300

    Use yq to generate snapcraft.yml from a template instead of patching the file with sed

--- snap.sh
+++ snap.sh
@@ -15,27 +15,20 @@ if [ "$(basename $P_DIR)" != "v2raya-snap" ]; then
 	echo -e "The script should be run from the v2raya-snap directory, instead of $PWD" >/dev/stderr
 	exit 2
 fi
-if [ -z "$(git --version)" ] || [ -z "$(wget --version)" ] || [ -z "$(snapcraft --version)" ]; then
-	echo "git, wget and snapcraft are required, but not installed"
+if [ -z "$(git --version)" ] || [ -z "$(wget --version)" ] || [ -z "$(snapcraft --version)" ] || [ -z $(yq --version) ]; then
+       echo "git, wget, yq and snapcraft are required, but not installed"
 	exit 1
 fi
 
-git checkout snap/snapcraft.yaml
-sed -i.backup "s/@VERSION@/$VERSION/g" snap/snapcraft.yaml
 declare readonly architectures=("x64") # Add your architectures here
 for ARCH in ${architectures[@]}; do
-	sed -i.backup-arch "s/@ARCH@/$ARCH/g" snap/snapcraft.yaml
+	yq -Y $".\"version\"=\"${VERSION}\" | .parts.v2raya.\"source\"=\"installer_debian_${ARCH}_${VERSION}.deb\"" snap/snapcraft.yaml.template > snap/snapcraft.yaml
 	if [ ! -e "installer_debian_${ARCH}_${VERSION}.deb" ]; then
 	wget "https://github.com/v2rayA/v2rayA/releases/download/v${VERSION}/installer_debian_${ARCH}_${VERSION}.deb" \
 	-O "$P_DIR/installer_debian_${ARCH}_${VERSION}.deb"
 	fi
 	snapcraft snap --output v2raya_${VERSION}_${ARCH}.snap
-	mv -f snap/snapcraft.yaml.backup-arch snap/snapcraft.yaml
 done
-
-
-mv -f snap/snapcraft.yaml.backup snap/snapcraft.yaml
-
 
 # Should publish snap here, but it's a really good idea to smoke test it by hand before pushing to the stable chanell
 #snapcraft export-login snapcraft-credfile
