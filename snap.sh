cf8709652ab1fc3bc876b6a6a48239106c46f497
Valeriy Manzhos <47538801+InvisibleRain@users.noreply.github.com>
2024-06-21 14:47:58 +0300

    Always get the latest version of v2ray-core from github

--- snap.sh
+++ snap.sh
@@ -23,7 +23,17 @@ fi
 export SNAPCRAFT_BUILD_ENVIRONMENT=multipass
 declare readonly architectures=("x64 arm64 riscv64") # Add your architectures here
 for ARCH in ${architectures[@]}; do
-	yq -Y $".\"version\"=\"${VERSION}\" | .parts.v2raya.\"source\"=\"installer_debian_${ARCH}_${VERSION}.deb\"" \
+	# Workaround around v2rayA and v2ray-core using different architecture names
+	if [[ $ARCH == "x64" ]]; then
+		v2ray_core_arch="64";
+	elif [[ $ARCH == "arm64" ]]; then
+		v2ray_core_arch="arm64-v8a";
+	else v2ray_core_arch=$ARCH;
+	fi
+	# Download the latest version of v2ray-core
+	v2ray_core_url=$(curl -sH "Accept: application/vnd.github.v3+json" https://api.github.com/repos/v2fly/v2ray-core/releases/latest |\
+		jq -r $".assets[].browser_download_url | select(test(\"v2ray-linux-${v2ray_core_arch}\\\.zip$\"))")
+	yq -Y $".version=\"${VERSION}\" | .parts.v2raya.source=\"installer_debian_${ARCH}_${VERSION}.deb\" | .parts.\"v2ray-core\".source=\"${v2ray_core_url}\"" \
 		snapcraft.yaml.template > snap/snapcraft.yaml
 	if [ ! -e "installer_debian_${ARCH}_${VERSION}.deb" ]; then
 	wget "https://github.com/v2rayA/v2rayA/releases/download/v${VERSION}/installer_debian_${ARCH}_${VERSION}.deb" \
